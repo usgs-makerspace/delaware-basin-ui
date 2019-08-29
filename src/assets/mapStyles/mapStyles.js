d80c97cce74386e9df8ebf0799eb48fe040a699a
Aaron Briggs <abriggs@contractor.usgs.gov>
2019-08-29 15:37:59 -0500

    changed back to using WBEEP for basetiles

--- src/assets/mapStyles/mapStyles.js
+++ src/assets/mapStyles/mapStyles.js
@@ -11,7 +11,7 @@ export default {
                 // If you are setting up a local build, you can uncomment the following
                 // URL assignment to pull the base tiles from S3 so that no local tile
                 // server is required:
-                // 'tiles': ['http://wbeep-test-website.s3-website-us-west-2.amazonaws.com/basetiles/{z}/{x}/{y}.pbf']
+                'tiles': ['http://wbeep-test-website.s3-website-us-west-2.amazonaws.com/basetiles/{z}/{x}/{y}.pbf']
             },
             delaware_basin_tiles: {
                 type: 'vector',
